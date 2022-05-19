b34cb02f5e249c054bf943b386524d3c4ae87734
Zi Qing Chua <ziqingchua@swuser-129-8-194-160.swlan.csufresno.edu>
2022-05-19 16:45:55 -0700

    finish exam-end

--- IS51Final.py
+++ IS51Final.py
@@ -24,3 +24,17 @@ print total number of grades
 print average grade
 print percentage of grades that are above the average 
 """
+
+infile = open("Final.txt", 'r')
+grades = [line.rstrip() for line in infile]
+infile.close()
+for i in range(len(grades)):
+    grades[i] = int(grades[i])
+average = sum(grades) / len(grades)
+num = 0 
+for grade in grades:
+    if grade > average:
+        num += 1
+print("Number of grades:", len(grades))
+print("Average grade:", average)
+print ("Percentafe of grade above average: {0:.2f}%".format(100 * num / len(grades)))
