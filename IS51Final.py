953d1cb5f42708d710aefef044bcac489bd19ec7
Zi Qing Chua <ziqingchua@swuser-129-8-194-160.swlan.csufresno.edu>
2022-05-19 16:01:09 -0700

    finish stuructured english

---
+++ IS51Final.py
@@ -0,0 +1,6 @@
+"""
+The program is attempting to show student grades on a final exam.
+The grades of 24 students are from a text file called "final.txt."
+It will show the number of grades, the average grade, and the percentage of hrades that are above the average grade
+Three different functions will be used to obtain three different grades.
+"""
